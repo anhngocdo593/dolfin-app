986576fca1abd64ad6f79d1b6b5e1bd2784eeac8
anhngocdohaha <anhngocdohaha@gmail.com>
2024-05-23 21:33:45 +0700

    Initial commit

---
+++ client/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-700">Styling just works!</Text>
+    </View>
+  );
+}
