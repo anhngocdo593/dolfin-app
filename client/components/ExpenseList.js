d7bc797b126dc65ee8a6f14d5dd285b95c52afcf
PvD <pvdzxc@gmail.com>
2024-06-02 23:49:31 +0700

    Move GET API to DefaultPage from ExpenseList

--- client/components/ExpenseList.js
+++ client/components/ExpenseList.js
@@ -14,46 +14,8 @@ const categoryImages = {
   // Thêm các ánh xạ khác tương ứng với các category khác
 };
 
-const ExpenseList = ({ handlePressItemEdit, expensesloading, setExpensesloading, day, month, year }) => {
-  const token = useSelector(state => state.token);
-  const [expensesdata, setExpensesdata] = useState(null)
-  const [error, setError] = useState(null);
-  useEffect (() =>{
-  async function getExpenses(url) {
-    var list = []
-    console.log(`fetching from ${url}`)
-    try{
-      const APIresponse = await fetch(url,{
-            method: 'GET',
-            headers: {
-              'Authorization': `Bearer ${token}`
-            }
-      });
-      if (!APIresponse.ok) {
-        throw new Error('Failed to fetch data');
-      }
-      const data = await APIresponse.json();
-      console.log('got data')
-      data.forEach(element => {
-        var item ={_id: element._id, date: element.date, category: element.category, amount: element.amount, description: element.description, time: element.time, userID: element.userID}
-        console.log(item)
-        list.push(item)
-      });
-      setExpensesdata(list)
-      setExpensesloading(false)
-    }
-    catch (error) {
-    setError(error.message);
-    throw new Error(error);
-  } 
-    // return APIresponse.json();
-  };
-  if (expensesloading){
-    console.log('loading expense')
-  getExpenses(`https://money-manager-ebon.vercel.app/expenses?day=${day}&month=${month}&year=${year}`)
-  setExpensesloading(false)
-  }}
-  )
+const ExpenseList = ({ expensesdata ,handlePressItemEdit, expensesloading, setExpensesloading, day, month, year }) => {
+  
   // try {
   //   console.log(token)
   //   const response = fetch(`https://money-manager-ebon.vercel.app/expenses?day=${day}&month=${month}&year=${year}`,{
